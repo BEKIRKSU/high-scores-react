f2f86bb578aa39fac02e586626f8e0bdd50a9ea4
BEKIRKSU <bekirkscu@gmail.com>
2023-06-13 05:59:01 -0700

    Update index.js

--- high-score-app/public/index.js
+++ high-score-app/public/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import allCountryScores from './public';
 
+function HighScoreTable({ country }) {
+    const scores = allCountryScores.find((item) => item.name === country).scores;
